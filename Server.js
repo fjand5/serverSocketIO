d75c7a63a80d94abad5d3615222ed16e03aabe88
fjand5 <huynhtheluat@gmail.com>
2017-07-23 09:54:07 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,3 +4,6 @@ io.on('connection', function (socket) {
   console.log("co nguoi ket noi: " + socket.id);
   socket.emit("YOUR_ID",socket.id);
 });
+io.on('LA_LANG', function (socket) {
+  socket.emit("YOUR_ID","tao nghe roi");
+});
