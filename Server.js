c3445276e8cedf74c792c4ff0ccffb0b7e5ae675
fjand5 <huynhtheluat@gmail.com>
2017-07-23 09:21:34 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,5 +2,5 @@ var io = require("socket.io")(process.env.PORT || 3000);
 console.log("Server da khoi dong v4")
 io.on('connection', function (socket) {
   console.log("co nguoi ket noi: " + socket.id);
-  socket.emit(socket.id);
+  socket.emit("YOUR_ID",socket.id);
 });
